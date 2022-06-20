b5c0e5068eb7c57d3865ccba198550abb0bf1775
Tanya Agarwal <tanya.agarwal@tanyaag-ltm98ca.internal.salesforce.com>
2022-06-20 16:53:30 +0530

    controller class implemented

--- src/main/java/com/example/springbootcrud/service/UserService.java
+++ src/main/java/com/example/springbootcrud/service/UserService.java
@@ -1,10 +1,11 @@
 package com.example.springbootcrud.service;
 
 import com.example.springbootcrud.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class UserService {
 
     private static List<User> list = new ArrayList<User>();
